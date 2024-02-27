053827cbe4705e1113a8a85abc16e6a91156caad
Cristian <cristian10damian@gmail.com>
2024-02-27 16:41:15 -0500

    Agregando docs

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import GifExpertApp from './GifExpertApp';
-<link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-  />
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
